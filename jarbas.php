823d7e3410b519a6c1d79909a9075ec7f9e6b516
Davi Alexandre <davi@davialexandre.com.br>
2018-09-01 19:33:50 -0300

    Add extension skeleton

---
+++ jarbas.php
@@ -0,0 +1,76 @@
+<?php
+
+require_once 'jarbas.civix.php';
+use CRM_Jarbas_ExtensionUtil as E;
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
+ */
+function jarbas_civicrm_config(&$config) {
+  _jarbas_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_xmlMenu().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
+ */
+function jarbas_civicrm_xmlMenu(&$files) {
+  _jarbas_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+ */
+function jarbas_civicrm_install() {
+  _jarbas_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
+ */
+function jarbas_civicrm_postInstall() {
+  _jarbas_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
+ */
+function jarbas_civicrm_uninstall() {
+  _jarbas_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
+ */
+function jarbas_civicrm_enable() {
+  _jarbas_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
+ */
+function jarbas_civicrm_disable() {
+  _jarbas_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
+ */
+function jarbas_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _jarbas_civix_civicrm_upgrade($op, $queue);
+}
